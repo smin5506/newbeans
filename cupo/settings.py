39ecdd6bdcd0fd8033e29709322542eb07c8e97f
smin5506 <smin@daegu.ac.kr>
2019-11-15 11:07:57 +0900

    commit crontab

--- cupo/settings.py
+++ cupo/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRONJOBS = [
-    ('*/3 11 * * *', 'newbeans.croller.check', '>> /home/msh9584/newbeans/file.log'),
-    ('*/3 11 * * *', 'newbeans.croller.savenogales', '>> /home/msh9584/newbeans/file.log'),
-    ('*/3 11 * * *', 'newbeans.croller.savenamu', '>> /home/msh9584/newbeans/file.log'),
-    ('*/3 11 * * *', 'newbeans.croller.savegsc', '>> /home/msh9584/newbeans/file.log'),
-    ('*/3 11 * * *', 'newbeans.croller.savelibre', '>> /home/msh9584/newbeans/file.log'),
+    ('10 11 * * *', 'newbeans.croller.check', '>> /home/msh9584/newbeans/file.log'),
+    ('10 11 * * *', 'newbeans.croller.savenogales', '>> /home/msh9584/newbeans/file.log'),
+    ('10 11 * * *', 'newbeans.croller.savenamu', '>> /home/msh9584/newbeans/file.log'),
+    ('10 11 * * *', 'newbeans.croller.savegsc', '>> /home/msh9584/newbeans/file.log'),
+    ('10 11 * * *', 'newbeans.croller.savelibre', '>> /home/msh9584/newbeans/file.log'),
 ]
