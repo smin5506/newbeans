0fe727facf56380a0446ab861fc905da9ab325f5
smin5506 <smin@daegu.ac.kr>
2019-11-15 11:56:05 +0900

    crontab

--- cupo/settings.py
+++ cupo/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRONJOBS = [
-    ('*/35 11 * * *', 'newbeans.croller.check', '>> /home/msh9584/newbeans/file.log 2>&1'),
-    ('*/35 11 * * *', 'newbeans.croller.savenogales', '>> /home/msh9584/newbeans/file.log 2>&1'),
-    ('*/35 11 * * *', 'newbeans.croller.savenamu', '>> /home/msh9584/newbeans/file.log 2>&1'),
-    ('*/35 11 * * *', 'newbeans.croller.savegsc', '>> /home/msh9584/newbeans/file.log 2>&1'),
-    ('*/35 11 * * *', 'newbeans.croller.savelibre', '>> /home/msh9584/newbeans/file.log 2>&1'),
+    ('0 12 * * *', 'newbeans.croller.check', '>> /home/msh9584/newbeans/file.log 2>&1'),
+    ('0 12 * * *', 'newbeans.croller.savenogales', '>> /home/msh9584/newbeans/file.log 2>&1'),
+    ('0 12 * * *', 'newbeans.croller.savenamu', '>> /home/msh9584/newbeans/file.log 2>&1'),
+    ('0 12 * * *', 'newbeans.croller.savegsc', '>> /home/msh9584/newbeans/file.log 2>&1'),
+    ('0 12 * * *', 'newbeans.croller.savelibre', '>> /home/msh9584/newbeans/file.log 2>&1'),
     
 ]
