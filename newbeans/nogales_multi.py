12fae81565b8d772ef16b756ed6679b211d43a6c
smin5506 <smin@daegu.ac.kr>
2019-11-21 01:27:04 +0900

    'multiprocessing'

---
+++ newbeans/nogales_multi.py
@@ -0,0 +1,109 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+from multiprocessing import Pool
+from datetime import date, timedelta
+
+def getData(url):
+	options = webdriver.ChromeOptions()
+	options.add_argument('headless')
+	options.add_argument('window-size=1920x1080')
+	options.add_argument("disable-gpu")
+	driver = webdriver.Chrome(r'C:/Users/SMIN/Downloads/chromedriver_win32/chromedriver', chrome_options=options)
+	driver.implicitly_wait(1)
+	driver.get(url)
+	html = driver.page_source
+	soup = BeautifulSoup(html, 'html.parser')
+	driver.close()
+	return soup
+
+#노갈레스	
+def nogales():
+	url = 'https://www.cafenogales.co.kr/shop'
+	beanUrl = 'https://www.cafenogales.co.kr/product/'
+
+	#저장할 변수
+	df = pd.DataFrame()
+	tmp = {}
+
+	soup = getData(url)
+
+	beanUrlList = soup.find_all('div', class_="shopProduct productName")
+
+	for beanCode in beanUrlList:
+		code, name, processing = beanCode.text.split(' / ')
+		
+		beanSoup = getData(beanUrl+code.replace('-',''))
+		beanRawData = beanSoup.find_all('span', class_='text')[0]
+		
+		for br in beanRawData.find_all("br"):
+			br.replace_with("\n")
+		
+		beanDataList = beanRawData.text.split('\n')
+		try:
+			tmp['price'] = beanSoup.find_all('span', class_='productPriceSpan')[0].text
+			for beanData in beanDataList:
+				beanData = beanData.replace('\xa0', ' ')
+				try:
+					key, val = beanData.split(' : ')
+					if key != 'Code':
+						tmp[key] = val
+					if key == 'Farmer':
+						tmp['Farm'] = val
+				except:
+					pass
+			df= df.append(tmp, ignore_index=True)
+			tmp = {}
+		except:
+			pass
+			
+	return df
+	
+def get_links():
+	url = 'https://www.cafenogales.co.kr/shop'
+	soup = getData(url)
+	beanUrlList = soup.find_all('div', class_="shopProduct productName")
+	
+	return beanUrlList
+	
+def get_content(beanCode):
+	tmp = {}
+	beanUrl = 'https://www.cafenogales.co.kr/product/'
+	code, name, processing = beanCode.text.split(' / ')
+		
+	beanSoup = getData(beanUrl+code.replace('-',''))
+	beanRawData = beanSoup.find_all('span', class_='text')[0]
+	
+	for br in beanRawData.find_all("br"):
+		br.replace_with("\n")
+	
+	beanDataList = beanRawData.text.split('\n')
+	try:
+		tmp['price'] = beanSoup.find_all('span', class_='productPriceSpan')[0].text
+		for beanData in beanDataList:
+			beanData = beanData.replace('\xa0', ' ')
+			try:
+				key, val = beanData.split(' : ')
+				if key != 'Code':
+					tmp[key] = val
+				if key == 'Farmer':
+					tmp['Farm'] = val
+			except:
+				pass
+	except:
+		pass
+	return tmp
+	
+
+if __name__=='__main__':
+
+	df = pd.DataFrame()
+	pool = Pool(processes=4) # 4개의 프로세스를 사용합니다.
+	result = pool.map(get_content, get_links()) 
+	df = df.append(result, ignore_index=True)
+	now = date.today().strftime('%y%m%d')
+	writer = pd.ExcelWriter('/home/msh9584/newbeans/xlsx/nogales'+now+'.xlsx', engine='xlsxwriter')
+	df.to_excel(writer, sheet_name='Sheet1')
+	writer.close()			
+
